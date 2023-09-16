8c968540b16e073e5606a1d5f6b6233e1e6cc4e4
rajasreegorrepati24@gmail.com <rajasreegorrepati24@gmail.com>
2023-09-16 12:42:29 +0530

    Intial commit

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import streamlit as st
 import pandas
 st.set_page_config(layout="wide")
-st.title("The Best Company")
+st.header("The Best Company")
 content=""" 
 A company is essentially an artificial person—also known as corporate personhood—in that it is an entity separate from the individuals who own, manage, and support its operations. 
 Companies are generally organized to earn a profit from business activities, but some may be structured as nonprofit charities. 
 Each country has its own hierarchy of company and corporate structures but with many similarities.
 """
 st.write(content)
-st.header("Our Team")
+st.subheader("Our Team")
 df = pandas.read_csv("data.csv")
-col1,col2,col3=st.columns([1.5,1.5,1.5])
+col1,col2,col3=st.columns(3)
 with col1:
     for index,row in df[:4].iterrows():
         st.subheader(f"{row['first name'].title()} {row['last name'].title()}")
